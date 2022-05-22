e03d3d08f9cff2417a08c5c5e080cd2b0431739f
AlirezaNorouzi <77004742+alirezan81@users.noreply.github.com>
2022-05-23 01:53:30 +0430

    9.5.0

--- view/template/postsTable.php
+++ view/template/postsTable.php
@@ -4,7 +4,7 @@
     </td>
     <td> <?php echo $post['id']; ?> </td>
     <td> <?php echo $post['subject']; ?> </td>
-    <td> <?php echo ($post['role'] == "admin" ? "ادمین" : "کاربر").' '.$post['writer']; ?> </td>
+    <td> <?php echo ($writer['role'] == "admin" ? "ادمین" : "کاربر").' '.$writer['fullname']; ?> </td>
     <td> <?php echo ($post['state'] == "saved" ? "ذخیره شده" : "منتشر شده"); ?> </td> 
     <td>
         <a class="" href="delete.php? id=<?php echo $post['id'] ?>&del=post">
