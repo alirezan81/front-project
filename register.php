ddbfbebc33b869bc205533c4781bb89d872be435
AlirezaNorouzi <77004742+alirezan81@users.noreply.github.com>
2022-05-23 02:22:37 +0430

    9.5.0

--- register.php
+++ register.php
@@ -10,30 +10,35 @@ if( isset( $_POST['submit'] ) ){
     $dbc = new DB( $db_host, $db_user, $db_pass, $db_name);
 
     $email = $_POST['email'];
-    $sql = "SELECT email FROM `users` WHERE email=?";
-    $result = $dbc->query( $sql,$email );
+    $sql = "SELECT email,status FROM `users` WHERE email=?";
+    $res = $dbc->query( $sql,$email );
     
-    if (!$row = $result->fetchArray()){
+    if(!$row = $res->fetchArray()){
         $sql = "INSERT INTO users(username,password,fullname,email,phone,gender) 
         VALUES(?, ?, ?, ?, ?, ?)";
 
         $result = $dbc -> query( $sql,$_POST['username'],$_POST['password'],$_POST['fullname'], $email, $_POST['phone'],$_POST['gender']);
-
-        $dbc -> close();
-
-        if($result){
-            $_SESSION['info'] = "<div style='color: darkgreen;'><p>شما با موفقیت ثبت نام شدید!</p></div>";
-            header("Location: register.php");
-        }else{
-            $_SESSION['info'] = "<div style='color: red;'><p>خطایی پیش آمد!</p></div>";
-            header("Location: register.php");
-        }
-    
-
-    }else{
-        $_SESSION['info'] = "<div style='color: red;'><p>کاربری با این ایمیل ثبت نام کرده است!</p></div>";
-        header("Location: register.php");
     }
+    else if($row['status'] == "delete"){
+        $sql = "UPDATE `users` SET status=? WHERE email=?";
+        $result = $dbc->query( $sql,'normal',$email);
+
+    }else{       
+        $_SESSION['info'] = "<div style='color: red;'><p>کاربری با این ایمیل ثبت نام کرده است!</p></div>";
+        $dbc -> close();
+        header("Location: register.php");
+        die();
+    }
+
+
+    if($result){
+        $_SESSION['info'] = "<div style='color: darkgreen;'><p>شما با موفقیت ثبت نام شدید!</p></div>";
+    }else{
+        $_SESSION['info'] = "<div style='color: red;'><p>خطایی پیش آمد!</p></div>";
+    }
+
+    header("Location: register.php");
+    $dbc -> close();
 
 
    
