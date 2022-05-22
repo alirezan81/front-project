c1b64f2f9a1d0b56db5cd65b05db9c39636c849f
AlirezaNorouzi <77004742+alirezan81@users.noreply.github.com>
2022-05-23 02:25:29 +0430

    9.5.0

--- sendTicket.php
+++ sendTicket.php
@@ -13,14 +13,10 @@ if(!Authentication::check()){
     
         $dbc = new DB( $db_host, $db_user, $db_pass, $db_name);
     
-        $sql = "SELECT email,fullname FROM `users` WHERE id=?";
-        $result = $dbc->query( $sql, $id);
-        $row = $result->fetchArray();
+        $sql = "INSERT INTO ticket(sender,section,title,description) 
+        VALUES(?,?,?,?)";
     
-        $sql = "INSERT INTO ticket(email,fullname,section,title,description) 
-        VALUES(?,?,?,?,?)";
-    
-        $result = $dbc -> query( $sql, $row['email'], $row['fullname'], $_POST['section'],
+        $result = $dbc -> query( $sql, $id, $_POST['section'],
         $_POST['title'], $_POST['description']);
     
         $dbc -> close();
