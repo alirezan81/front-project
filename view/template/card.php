261fe6ce6d8b2552aa074a388c3319a6c8ffc4f1
AlirezaNorouzi <77004742+alirezan81@users.noreply.github.com>
2022-05-23 01:52:31 +0430

    9.5.0

--- view/template/card.php
+++ view/template/card.php
@@ -2,7 +2,7 @@
     <img class="card-img-top" src="cover/<?php echo $row['cover']; ?>" alt="تصویر کاور">
     <div class="card-body">
         <h3 class="card-title"><?php echo $row['subject']; ?></h3>
-        <p class="card-sub-head"><?php echo ($row['role'] == "admin" ? "ادمین" : "کاربر")." ".($row['writer']); ?></p>
+        <p class="card-sub-head"><?php echo ($writer['role'] == "admin" ? "ادمین" : "کاربر")." ".($writer['fullname']); ?></p>
         <p class="card-sub-head">زمان مطالعه</p>
         <p class="card-sub-head">پیش نیاز</p>
         <p class="card-text">محتوا</p>
